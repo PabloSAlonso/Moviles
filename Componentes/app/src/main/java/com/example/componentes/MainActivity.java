5b51b534359987cdfdc2cb8909e87d4c17904c7c
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-09-30 13:47:12 +0200

    changes

--- Componentes/app/src/main/java/com/example/componentes/MainActivity.java
+++ Componentes/app/src/main/java/com/example/componentes/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.componentes;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +21,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TextView txtView;
+    Button btn1;
+    Button btnImagen;
+    ImageButton imgBtn;
+    ToggleButton tglBtn;
+    CheckBox chBox1;
+    CheckBox chBox2;
+    CheckBox chBox3;
+    RadioButton rdBtn1;
+    RadioButton rdBtn2;
+    Switch switch1;
+    SeekBar seekBar1;
+    RatingBar ratingBar1;
+    EditText editText1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +46,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        toolbar = findViewById(R.id.toolBar);
+        txtView = findViewById(R.id.txtView);
+        btn1 = findViewById(R.id.btn1);
+        btnImagen = findViewById(R.id.btnImagen);
+        imgBtn = findViewById(R.id.imgBtn);
+        tglBtn = findViewById(R.id.tglBtn);
+        chBox1 = findViewById(R.id.chBox1);
+        chBox2 = findViewById(R.id.chBox2);
+        chBox3 = findViewById(R.id.chBox3);
+        rdBtn1 = findViewById(R.id.rdBtn1);
+        rdBtn2 = findViewById(R.id.rdBtn2);
+        switch1 = findViewById(R.id.switch1);
+        seekBar1 = findViewById(R.id.seekBar1);
+        ratingBar1 = findViewById(R.id.ratingBar1);
+        editText1 = findViewById(R.id.editText1);
     }
 }
