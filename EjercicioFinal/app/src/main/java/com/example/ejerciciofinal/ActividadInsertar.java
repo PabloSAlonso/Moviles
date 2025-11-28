7677ebcc1392d921783a291675e1c9458c1e2ccf
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-11-28 11:20:37 +0100

    changes

--- EjercicioFinal/app/src/main/java/com/example/ejerciciofinal/ActividadInsertar.java
+++ EjercicioFinal/app/src/main/java/com/example/ejerciciofinal/ActividadInsertar.java
@@ -1,6 +1,13 @@
 package com.example.ejerciciofinal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +17,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ActividadInsertar extends AppCompatActivity {
 
+    EditText etNombre, etDirector, etDuracion;
+    Spinner spinnerSala;
+    ArrayAdapter<String> adaptadorSpinner;
+    RadioButton rb1, rb2, rb3, rb4;
+    CalendarView calendarioFechas;
+    Pelicula nuevaPelicula;
+    Button guardar;
+    String[] salasSpinner = {"Sala 1", "Sala 2", "Sala 3", "Sala 4", "Sala 5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +34,25 @@ public class ActividadInsertar extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        etNombre = findViewById(R.id.etTituloInsertar);
+        etDirector = findViewById(R.id.etDirectorInsertar);
+        etDuracion = findViewById(R.id.etDuracioInsertar);
+        spinnerSala = findViewById(R.id.spinnerSalaInsertar);
+        rb1 = findViewById(R.id.rb1Insertar);
+        rb2 = findViewById(R.id.rb2Insertar);
+        rb3 = findViewById(R.id.rb3Insertar);
+        rb4 = findViewById(R.id.rb4Insertar);
+        guardar = findViewById(R.id.btnGuardar);
+        adaptadorSpinner = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, salasSpinner);
+        spinnerSala.setAdapter(adaptadorSpinner);
+        calendarioFechas = findViewById(R.id.calendarioFechaInsertar);
+        nuevaPelicula = new Pelicula(etNombre.getText(), etDirector.getText(),Integer.parseInt(etDuracion.getText()+""), calendarioFechas.getDate(), spinnerSala, , );
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
